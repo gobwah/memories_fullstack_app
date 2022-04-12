c8a1bb39efce2b7f0e3b7555d82c9a8a5e743721
Vincent Della Libera <supervince40@hotmail.fr>
2022-04-13 00:17:43 +0200

    remove dead code

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,12 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import './index.css'
-// import dotenv from 'dotenv'
 
 import reducers from './reducers'
 
 import App from './App'
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)))
-// dotenv.config()
 
 ReactDOM.render(
     <Provider store={store}>
